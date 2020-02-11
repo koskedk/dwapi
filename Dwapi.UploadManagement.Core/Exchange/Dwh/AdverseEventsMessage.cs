fa47a4d9c82d7be60703e9eafd25290b236613f5
Koske Kimutai <koskedk@live.com>
2020-02-11 14:25:49 +0300

    initial adversevent messagebag

--- Dwapi.UploadManagement.Core/Exchange/Dwh/AdverseEventsMessage.cs
+++ Dwapi.UploadManagement.Core/Exchange/Dwh/AdverseEventsMessage.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Dwapi.UploadManagement.Core.Interfaces.Exchange;
+using Dwapi.UploadManagement.Core.Interfaces.Exchange.Dwh;
 using Dwapi.UploadManagement.Core.Model.Dwh;
+using Newtonsoft.Json;
 
 namespace Dwapi.UploadManagement.Core.Exchange.Dwh
 {
-    public class AdverseEventsMessage
+    public class AdverseEventsMessage:IAdverseEventsMessage
     {
         public Facility Facility
         {
@@ -17,6 +21,20 @@ namespace Dwapi.UploadManagement.Core.Exchange.Dwh
             }
         }
         public PatientExtractView Demographic { get; set; }
+        [JsonProperty(PropertyName = "ArtExtracts")]
+        public List<PatientAdverseEventView> Extracts { get; }
+
+        public List<Guid> SendIds { get; }
+        public IMessage<PatientAdverseEventView> Generate(PatientAdverseEventView extract)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IMessage<PatientAdverseEventView>> GenerateMessages(List<PatientAdverseEventView> extracts)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<PatientAdverseEventView> AdverseEventExtracts { get; set; } = new List<PatientAdverseEventView>();
 
         public AdverseEventsMessage()
